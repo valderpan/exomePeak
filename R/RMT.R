19048a13db86694a6875e2b0d2c482755ddd8033
Pan Haoran <51531508+valderpan@users.noreply.github.com>
2026-03-31 21:18:52 +0800

    Update txdb creation to use txdbmaker package
    Change `makeTxDbFromGFF()` to `txdbmaker::makeTxDbFromGFF()` to meet the requirements of GenomicFeatures >= 1.61.1

--- R/RMT.R
+++ R/RMT.R
@@ -242,7 +242,7 @@ RMT <- function(
   # use provided annotation data file
   if (!is.na(PARAMETERS$GENE_ANNO_GTF) & is.na(PARAMETERS$TXDB) ) {
     op <- options(warn = (-1))
-    txdb=makeTxDbFromGFF(PARAMETERS$GENE_ANNO_GTF,format="gtf")
+    txdb=txdbmaker::makeTxDbFromGFF(PARAMETERS$GENE_ANNO_GTF,format="gtf")
     options(op)
   }
 
